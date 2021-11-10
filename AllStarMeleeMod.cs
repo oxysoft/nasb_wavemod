ab01fe81384bba4555d88216267477ac6c8eed71
oxysoft <teoxysoft@gmail.com>
2021-11-10 03:45:17 -0500

    Initial commit

---
+++ AllStarMeleeMod.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace MyNameSpace // Rename "MyNameSpace"
+{
+	[BepInPlugin(ID, NAME, VERSION)]
+	public class AllStarMeleeMod : BaseUnityPlugin
+	{
+		private const string ID      = "com.author.project";
+		private const string NAME    = "All Star Melee";
+		private const string VERSION = "1.0";
+
+		public static AllStarMeleeMod Instance { get; private set; }
+
+		public ManualLogSource logger => base.Logger;
+
+		public new static ManualLogSource Logger => Instance.logger;
+
+		internal void Awake()
+		{
+			Instance = this;
+
+			Logger.Log(LogLevel.Message, $"{NAME} {VERSION}");
+
+			var harmony = new Harmony(NAME);
+			harmony.PatchAll();
+		}
+	}
+}
